2da9d8b55819cff910ca4d19a002c48924969197
valebolo93 <valebolo93@gmail.com>
2025-02-19 18:02:16 +0100

    Ex3.2

---
+++ Lezione_05/Ex3.2.py
@@ -0,0 +1,7 @@
+'''3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name.'''
+
+names:list = ["Valentina","Lorenzo","Leandro","Stefano", "Francesco"]
+
+for nome in names:
+    print(f"ciao,{nome} come stai?")
