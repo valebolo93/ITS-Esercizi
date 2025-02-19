7a542bc6dc87f6b5af2cc278a991e1c28fb61840
valebolo93 <valebolo93@gmail.com>
2025-02-19 18:00:48 +0100

    esercizio1-4

--- Lezione_02/Dato_Operatori/esercizio1-4.py
+++ Lezione_02/Dato_Operatori/esercizio1-4.py
@@ -5,3 +5,13 @@ print(mylist[0])
 print(mylist[1])
 print(mylist[2])
 print(mylist[3])
+
+n=int(input("Inserisci un numero di 4 cifre"))
+m = n //1000
+c = n //100 - m * 10
+d = n //10 - m * 100 - c * 10
+u = n - m * 1000 - c * 100 - d * 10
+print(m)
+print(c)
+print(d)
+print(u)
