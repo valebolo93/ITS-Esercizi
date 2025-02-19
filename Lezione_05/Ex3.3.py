ab30cf0aef89f25f21b629b64c9a825b38a27f27
valebolo93 <valebolo93@gmail.com>
2025-02-19 19:14:08 +0100

    Ex3.3

--- Lezione_05/Ex3.3.py
+++ Lezione_05/Ex3.3.py
@@ -1,7 +1,17 @@
 '''Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
 Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”'''
 
-mylist:list= ["motorbike", "car", "plane"]
+mylist:list[str]= ["motorbike", "car", "plane"]
 
 for mezzi in mylist:
     print(f"I would like to own a {mezzi}")
+else:
+    print(f"Finished")
+
+#exercise
+mylist2:list[str]= ["motorbike", "car", "plane"]
+print(f"I would like to own a {mylist2[0]}")
+print(f"I would like to own a {mylist2[1]}")
+print(f"I would like to own a {mylist2[2]}")
+
+
