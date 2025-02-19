91701f7db0079eb8d83e787279ca60c0ebe64c60
valebolo93 <valebolo93@gmail.com>
2025-02-19 17:35:56 +0100

    Ex2.6

---
+++ Lezione_05/Ex2.6.py
@@ -0,0 +1,6 @@
+#Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and 
+# represent it with a new variable called message. Print your message. 
+
+famous_person:str= "Alda Merini"
+message:str= "'L'uomo impara sempre a vivere quando è troppo tardi!'"
+print(str(famous_person + " once said:" + message))
