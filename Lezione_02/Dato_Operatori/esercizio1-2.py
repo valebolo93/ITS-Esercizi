59156d28b70de5a30158b4deca75e4def300df12
valebolo93 <valebolo93@gmail.com>
2025-02-19 17:30:43 +0100

    esercizio1-2

--- Lezione_02/Dato_Operatori/esercizio1-2.py
+++ Lezione_02/Dato_Operatori/esercizio1-2.py
@@ -3,3 +3,14 @@ x: float = 3.2
 y: float = x%2.0
 print (x)
 print (y)
+
+print(f"{x:.3f}")
+print(f"{y:.3f}")
+
+x: float = -3.2 
+y: float = x%2.0
+print (x)
+print (y)
+
+print(f"{x:.3f}")
+print(f"{y:.3f}")
