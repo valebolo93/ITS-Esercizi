d2d2093253d125ecef5073586e44857ac92711c6
valebolo93 <valebolo93@gmail.com>
2025-02-19 18:06:55 +0100

    esercizio1-5

--- Lezione_02/Dato_Operatori/esercizio1-5.py
+++ Lezione_02/Dato_Operatori/esercizio1-5.py
@@ -1,4 +1,4 @@
 #eser 1-5
 gradi_fahrenheit: int = 72
 gradi_Celsius = 5*(gradi_fahrenheit - 32)/9
-print(gradi_fahrenheit)
+print(f"{gradi_fahrenheit} gradi Fahreneit corrispondono a {gradi_Celsius:.1f} gradi Celsius")
