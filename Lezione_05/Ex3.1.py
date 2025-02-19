34bd126986d958a710dcb9da09197d40e259bd0e
valebolo93 <valebolo93@gmail.com>
2025-02-19 18:01:09 +0100

    Ex3.1

--- Lezione_05/Ex3.1.py
+++ Lezione_05/Ex3.1.py
@@ -1,7 +1,8 @@
 #3-1. Names: Store the names of a few of your friends in a list called names. 
 # Print each person’s name by accessing each element in the list, one at a time.
 
-names:str= ["Valentina","Lorenzo","Leandro","Stefano", "Francesco"]
+names:list= ["Valentina","Lorenzo","Leandro","Stefano", "Francesco"]
+
 print(names[0])
 print(names[1])
 print(names[2])
