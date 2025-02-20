92211e523dc71eb7eb9c791cbe5e642ffb13a2e5
valebolo93 <valebolo93@gmail.com>
2025-02-20 15:24:28 +0100

    Ex.3.9

---
+++ Lezione_05/Ex.3.9.py
@@ -0,0 +1,5 @@
+'''3-9. Dinner Guests: Working with one of the programs from Exercises 3, 
+use len() to print a message indicating the number of people you’re inviting to dinner.'''
+
+mylist:list[str]= ["Totti", "Berlinguer", "Ghandi"]
+print(len(mylist))
