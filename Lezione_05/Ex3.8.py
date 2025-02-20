cc488d52889cbfbc2fcd15c0711fa2dc0452e78a
valebolo93 <valebolo93@gmail.com>
2025-02-20 15:20:27 +0100

    Ex.3.8

---
+++ Lezione_05/Ex3.8.py
@@ -0,0 +1,27 @@
+'''3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a list. Make sure the list is not in alphabetical order.
+• Print your list in its original order. Don’t worry about printing the list neatly; just print it as a raw Python list.
+• Use sorted() to print your list in alphabetical order without modifying the actual list.
+• Show that your list is still in its original order by printing it.
+• Use sorted() to print your list in reverse-alphabetical order without changing the order of the original list.
+• Show that your list is still in its original order by printing it again.
+• Use reverse()  to change the order of your list. Print the list to show that its order has changed.
+• Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+• Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+• Use sort() to change your list so it’s stored in reverse-alphabetical order.
+Print the list to show that its order has changed.'''
+
+my_list_places:list[str]= ["Japan", "Australia", "Island", "Peru", "Egypt"]
+print(my_list_places)
+ordinata = sorted(my_list_places)
+print(ordinata)
+print(my_list_places)
+
+ordinata = sorted(my_list_places, reverse=True)
+print(ordinata)
+
+ordinata = sorted(my_list_places, reverse=False)
+print(ordinata)
+
+ordinata = sorted(my_list_places)
+print(my_list_places)
