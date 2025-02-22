505900f620c11d0eba27b299b19b54dbbde570d5
valebolo93 <valebolo93@gmail.com>
2025-02-22 11:11:50 +0100

    Ex4.4

---
+++ Lezione_06/Ex4.4.py
@@ -0,0 +1,9 @@
+'''4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
+ (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)'''
+
+
+lista_numeri= list(range(1,1000001))
+
+for i in range(1,1000001):
+    print(i)
+    
