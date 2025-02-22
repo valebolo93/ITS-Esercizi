5d42342f8bfb973f45248693be0cdc82645f3ebf
valebolo93 <valebolo93@gmail.com>
2025-02-22 16:36:45 +0100

    esercizio2

---
+++ Compito/esercizio2.py
@@ -0,0 +1,5 @@
+'''Scrivere un programma che acquisisca una stringa inserita dall'utente e calcoli il numero 
+totale di spazi presenti nella stringa. Il risultato deve essere visualizzato in output.'''
+
+parola=(input())
+print(len(parola))
