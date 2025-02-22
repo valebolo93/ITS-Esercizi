a4ebbe301635b409a375ffd81ac68fd72f1988b3
valebolo93 <valebolo93@gmail.com>
2025-02-22 15:45:13 +0100

    Ex4.6

---
+++ Lezione_06/Ex4.6.py
@@ -0,0 +1,9 @@
+'''4-6. Odd Numbers: Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. 
+Use a for loop to print each number.'''
+
+range:list=[1,2]
+numeri_dispari=(any(range(1,21,2)))
+step=2
+print(numeri_dispari(step))
+
+    
