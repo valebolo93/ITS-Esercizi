d9852dccd8b59bdf6ca96e1cb0f1b4493c6cadef
valebolo93 <valebolo93@gmail.com>
2025-02-24 18:05:40 +0100

    esercizio3

--- Compito/esercizio3.py
+++ Compito/esercizio3.py
@@ -2,7 +2,10 @@
 che corrisponda alla versione invertita della stringa originale. Il programma deve poi visualizzare la stringa ottenuta in output. 
 Per risolvere il problema non si deve utilizzare alcun tipo di funzione, ma esclusivamente i cicli.'''
 
+#Ho inserito una prima stringa con l'input che verrà dato dall'ultente
 stringa:str= (input("Inserisci una stringa: "))
+
+#Ho poi utilizzato un ciclo for e inizializzato una nuova variabile stringa per andare a invertire quella originale
 for parola in stringa:
     stringa1= stringa[::-1]
     print(stringa1)
