d1e08387ed6c631ed0bab73775287cf024f6e87b
valebolo93 <valebolo93@gmail.com>
2025-02-26 19:54:02 +0100

    Ex4.11

--- Lezione_06/Ex4.11.py
+++ Lezione_06/Ex4.11.py
@@ -5,14 +5,17 @@
 • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a 
 for loop to print the first list. Print the message My friend’s favorite pizzas are:,
  and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.'''
-
+#lista originale
 lista:list[str]= ["Margherita","Diavola","Patate e Salsicce"]
-friend_pizzas=lista
+
+#copia della lista
+friend_pizzas=lista[:]
 print(friend_pizzas)
 
+#ho aggiunto un nuovo elemento alla seconda lista
 friend_pizzas.append("Marinara")
 
-print(f"My favourite pizzas are: {*lista}")
+print(f"My favourite pizzas are: {lista}")
 
 for i in friend_pizzas:
-    print(i)
+    print(f"My favourite pizzas are: {i}")
