9ef2597bfe67b56523827e760b98c046a4bd2dc2
valebolo93 <valebolo93@gmail.com>
2025-02-26 17:44:41 +0100

    Ex4.7

--- Lezione_06/Ex4.7.py
+++ Lezione_06/Ex4.7.py
@@ -1,4 +1,12 @@
 '''4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.'''
 
+#modo 1 solo col ciclo for
+
 for i in range(3,31,3):
+        print(i)
+
+#modo 2 con la lista e ciclo for
+
+lista:list=[3,6,9,12,15,18,21,24,27,30]
+for i in lista:
         print(i)
