8eb76f090f19fe3772cd5e1170a02892743ac781
valebolo93 <valebolo93@gmail.com>
2025-02-28 13:52:33 +0100

    Ex5.5

---
+++ Lezione_06/Ex5.5.py
@@ -0,0 +1,34 @@
+'''5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.'''
+
+alien_color= "green"
+
+if alien_color==alien_color:
+    print("Player earned 5 points")
+elif alien_color=="Yellow":
+    print("Player earned 10 points")
+else:
+    print("Player earned 15 points")
+print("----------------------------------------------------------------------------------------------------------------------")
+alien_color1= "yellow"
+
+if alien_color!=alien_color:
+    print("Player earned 5 points")
+elif alien_color1==alien_color1:
+    print("Player earned 10 points.")
+else:
+    print("Player earned 15 points")
+print("-----------------------------------------------------------------------------------------------------------")
+alien_color2="red"
+
+if alien_color2==alien_color2:
+    print("Player earned 15 points")
+elif alien_color2=="Yellow":
+    print("Player earned 10")
+else:
+    print("Player earned 5 points")
+
