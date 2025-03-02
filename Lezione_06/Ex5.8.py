c10c18dd2159ee84e24857c89b1f8ab5a2f0445a
valebolo93 <valebolo93@gmail.com>
2025-03-02 12:33:03 +0100

    Ex5.8

---
+++ Lezione_06/Ex5.8.py
@@ -0,0 +1,19 @@
+'''Hello Admin: Make a list of five or more usernames, including the name 'admin'.
+ Imagine you are writing code that will print a greeting to each user after they log in to a website.
+Loop through the list, and print a greeting to each user.
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+• Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.'''
+
+name:list=["admin","Vale","Stefano","Sofi","Mik"]
+
+for i in name:
+    if name[1:]:
+        print(f"Hello {name[1]}, thank you for logging in again")
+        print(f"Hello {name[2]}, thank you for logging in again")
+        print(f"Hello {name[3]}, thank you for logging in again")
+        print(f"Hello {name[4]}, thank you for logging in again")
+    break
+
+if name[1]:
+    print(f"Hello {name[0]}  would you like to see a status report?")
+   
