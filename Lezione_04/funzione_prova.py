a2e5f361e824af36847fc46ae9d7aaaa25b8badd
valebolo93 <valebolo93@gmail.com>
2025-03-04 19:16:35 +0100

    funzione_prova

--- Lezione_04/funzione_prova.py
+++ Lezione_04/funzione_prova.py
@@ -5,14 +5,15 @@ def substract(a:int,b:int):
 
 mysubstract= substract(10,5)
 print(mysubstract)
-
+print("---------------------------------------------------------------------")
 #con input
 
-x:int=input("Inserire numero: ")
-x:int=input("Inserire numero: ")
+x:int=int(input("Inserire numero: "))
+g:int=int(input("Inserire numero: "))
 
-def substract(x:int,y:int)->int:
-    result:int = x - y
+def substract(x:int,g:int)->int:
+    result:int = x - g
     return result
 
-mysubstract1= substract(x,y)
+mysubstract1= substract(x,g)
+print(mysubstract1)
