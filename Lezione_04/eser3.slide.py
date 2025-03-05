c8961543e2fa5918690063e7a544e683c85c81f7
valebolo93 <valebolo93@gmail.com>
2025-03-05 18:34:30 +0100

    eser3.slide

---
+++ Lezione_04/eser3.slide.py
@@ -0,0 +1,11 @@
+'''Write a function print_numbers(), which takes a list of numbers as argument
+Using a for loop, print each number one by one.'''
+
+def print_numbers(lista:list):
+  for i in lista:
+    print(i)
+
+lista:list[int]=[1,2,3,4,5,6,7,8,9,10]
+
+print_numbers(lista)
+
