08b1d346cb04b6bbf8473d97067a2a67c416cf34
valebolo93 <valebolo93@gmail.com>
2025-03-07 19:13:57 +0100

    eser8.9

---
+++ Lezione_09/Eser8.9.py
@@ -0,0 +1,14 @@
+'''Make a list containing a series of short text messages. 
+Pass the list to a function called show_messages(), which prints each text message.'''
+lista:list=["Come va?"
+            "Ti voglio bene"
+            "Forza Roma"
+            "W l'estate"]
+
+
+def show_messages(lista):
+    print(lista[0])
+    print(lista[1])
+    print(lista[2])
+
+print(show_messages())
