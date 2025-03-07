04382a82861d30350b19be67f381e8190cc457a6
valebolo93 <valebolo93@gmail.com>
2025-03-07 14:58:18 +0100

    funzione-prova

---
+++ Lezione_04/funzione-prova.py
@@ -0,0 +1,6 @@
+'''Scrivi una funzione che prende una stringa e restituisce la stringa invertita.'''
+
+def stringa_invertita(parola:str):
+    return (parola[::-1])
+
+print(stringa_invertita("Ciao"))
