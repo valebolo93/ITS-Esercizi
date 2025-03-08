21dda81ca4ad856b58d3e9203ea418546308fe51
valebolo93 <valebolo93@gmail.com>
2025-03-08 13:48:30 +0100

    Eser8.9

--- Lezione_09/Eser8.8.py
+++ Lezione_09/Eser8.8.py
@@ -3,17 +3,16 @@
 call make_album() with the user’s input and print the dictionary that’s created. 
 Be sure to include a quit value in the while loop.'''
 
-
-
 def make_album(album:str, artist:str, num_songs:int=None) ->dict:
      if num_songs is None:
         num_songs= "Unknown"
-        return {"Album":album, "Artista":artist, "num_songs":num_songs}
+        return {"Album":album, "artista":artist, "num_songs":num_songs}
 
 while True:
     insert_album:str=input("Inserisci un album: ")
     insert_artist:str=input("Inserire nome artista: ")
     break
+
 
 
 print(make_album(insert_album,insert_artist))
--- Lezione_09/Eser8.9.py
+++ Lezione_09/Eser8.9.py
@@ -1,14 +1,15 @@
 '''Make a list containing a series of short text messages. 
 Pass the list to a function called show_messages(), which prints each text message.'''
-lista:list=["Come va?"
-            "Ti voglio bene"
-            "Forza Roma"
+lista:list=["Come va?",
+            "Ti voglio bene",
+            "Forza Roma",
             "W l'estate"]
 
 
 def show_messages(lista):
-    print(lista[0])
-    print(lista[1])
-    print(lista[2])
+    return lista
 
-print(show_messages())
+print(lista[0])
+print(lista[1])
+print(lista[2])
+print(lista[3])
