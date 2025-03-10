76b4cadffca4374a9342014fef82209bab199c9f
valebolo93 <valebolo93@gmail.com>
2025-03-10 09:41:26 +0100

    Eser8.10

---
+++ Lezione_09/Eser8.10.py
@@ -0,0 +1,31 @@
+''' Start with a copy of your program from Exercise 8-9. Write a function called send_messages() 
+that prints each text message and moves each message to a new list called sent_messages as it’s printed. 
+After calling the function, print both of your lists to make sure the messages were moved correctly.'''
+
+#lista esistente
+lista:list=["Come va?",
+            "Ti voglio bene",
+            "Forza Roma",
+            "W l'estate"]
+called_messages:list=[]
+
+
+
+#funzione
+def send_messages(lista):
+    return called_messages.append[0](items)
+
+for items in lista:
+    called_messages.append(items)
+
+print(called_messages)
+print(lista)
+
+
+
+
+
+ 
+    
+
+
