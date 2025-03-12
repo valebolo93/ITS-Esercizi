0a4a6d4ecbb40bc089ffe6c1e3ccb26fdd779448
valebolo93 <valebolo93@gmail.com>
2025-03-12 13:45:43 +0100

    function6

---
+++ By_myself/Functios/function6.py
@@ -0,0 +1,20 @@
+'''Write a program to create a recursive function to calculate the sum of numbers from 0 to 10.
+
+A recursive function is a function that calls itself again and again.
+
+Expected Output:
+
+55'''
+
+i=1
+somma=0
+for i in range (11):
+    somma=somma + i
+
+print(somma)
+
+
+
+
+        
+        
