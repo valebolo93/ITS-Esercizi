84b658bdf7683b78dc658367d9c3268e8033ea5a
valebolo93 <valebolo93@gmail.com>
2025-03-12 12:33:31 +0100

    Ex5.11

--- Lezione_06/Ex5.11.py
+++ Lezione_06/Ex5.11.py
@@ -5,3 +5,14 @@
 Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.'''
 
 
+ordinal_numbers:list = [1,2,3,4,5,6,7,8,9]
+
+for i in ordinal_numbers:
+    if i == 1:
+        print(f"{i}th!")
+    elif i == 2:
+        print(f"{i}nd!")
+    elif i == 2:
+        print(f"{i}3d!")
+    else:
+        print(f"{i}nd!")
