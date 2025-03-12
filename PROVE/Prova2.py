99bd1d020677286151dafd9b95a323aa077f08d6
valebolo93 <valebolo93@gmail.com>
2025-03-12 13:53:30 +0100

    Prova2

--- PROVE/Prova2.py
+++ PROVE/Prova2.py
@@ -1,2 +1,5 @@
-def asRoma(giorno:int,mese:str,anno:int,kwarg*) -> None:
+def asRoma(giorno:int,mese:str,anno:int) -> None:
     return f"La mia squadra è nata il {giorno}/{mese}/{anno}"
+
+print(asRoma(27,"Luglio",1927))
+
