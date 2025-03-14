b1c62a03b54f969f4e14c690f315a04716e21fc4
valebolo93 <valebolo93@gmail.com>
2025-03-14 11:10:28 +0100

    Eser9.2

---
+++ Lezione_10/Eser9.2.py
@@ -0,0 +1,27 @@
+'''Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, 
+and call describe_restaurant() for each instance.'''
+
+class Restaurant:
+
+    def __init__(self,restaurant_name:str,cuisin_type:str):
+        self.restaurant_name=restaurant_name
+        self.cuisin_type=cuisin_type
+
+    def descrive_restaurant(self):
+        print(f"{self.restaurant_name}: {self.cuisin_type}")
+    
+    def open_restaurant(self):
+        print("The restaurant is open")
+
+restaurant:Restaurant= Restaurant("Da Gigi","romana")
+restaurant1:Restaurant= Restaurant("Marechiaro","pesce")
+restaurant2:Restaurant= Restaurant("Pizzenfaccia","pizzeria")
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisin_type)
+print(restaurant1.restaurant_name)
+print(restaurant1.cuisin_type)
+print(restaurant2.restaurant_name)
+print(restaurant2.cuisin_type)
+
+restaurant.descrive_restaurant()
