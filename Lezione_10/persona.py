0ac3f597c27f8bce71cba9628d89f5f47d4a75d6
valebolo93 <valebolo93@gmail.com>
2025-03-20 15:18:33 +0100

    persona

---
+++ Lezione_10/persona.py
@@ -0,0 +1,16 @@
+class Persona:
+    '''Di una persona voglio sapere delle informazioni.
+    Queste info vengono chiamare attributi(della classe Persona)
+    Le info saranno:
+    -nome,cognome,età.
+    Come li rappresento?
+    self.name:str(attributo di tipo stringa)
+    self.lastname:str(attributo di tipo stringa)
+    self.age:int(attributo di tipo int)'''
+
+    #costruttore
+    def __init__(self,name:str,last_name:str,age:int): #self significa che mi riferisco alla classe Persona
+        self.name:str=name
+        self.last_name:str=last_name
+        self.age:int=age
+        
