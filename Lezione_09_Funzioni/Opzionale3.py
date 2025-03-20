444f55098b0c9d97593a56d12d995b9f3a89dbcc
valebolo93 <valebolo93@gmail.com>
2025-03-20 15:01:42 +0100

    opzionale3

---
+++ Lezione_09_Funzioni/Opzionale3.py
@@ -0,0 +1,13 @@
+'''
+    Create a function that defines a product with a name, price, and quantity.
+     Create a function that manages the shopping cart, allowing the user to add, remove,
+     and view products in the cart.
+    The function should calculate the cart total and apply any discounts or taxes.
+     Implement a for loop to iterate over the items in the cart and print detailed information about each product and the total.
+'''
+
+def product_name(name:str,price:float,quantity:float)->None:
+    return f"Nome prodotto: {name},prezzo: {price},quantità: {quantity}"
+
+def shopping_cart(prodotto:"name")->dict:
+    mydict:dict={prosciutto:Parma}
