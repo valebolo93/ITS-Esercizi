e83f46350899820fa2a89723b3daea3e531c3f4a
valebolo93 <valebolo93@gmail.com>
2025-03-24 09:35:39 +0100

    Opzionale3

--- Lezione_09_Funzioni/Opzionale3.py
+++ Lezione_09_Funzioni/Opzionale3.py
@@ -9,5 +9,13 @@
 def product_name(name:str,price:float,quantity:float)->None:
     return f"Nome prodotto: {name},prezzo: {price},quantità: {quantity}"
 
-def shopping_cart(prodotto:"name")->dict:
-    mydict:dict={prosciutto:Parma}
+def shopping_cart():
+    return [products]
+
+products:list=[]
+
+select:str=input(" Insert: Add,remove or view products: ")
+
+match select:
+    case "add":
+        insert:str=input("Which product would you like to insert?")
