82e2db1f05a8f4c00f862655df13b2b54ae48ee1
valebolo93 <valebolo93@gmail.com>
2025-03-25 11:30:00 +0100

    opzionale3

--- Lezione_09_Funzioni/Opzionale3.py
+++ Lezione_09_Funzioni/Opzionale3.py
@@ -5,17 +5,30 @@
     The function should calculate the cart total and apply any discounts or taxes.
      Implement a for loop to iterate over the items in the cart and print detailed information about each product and the total.
 '''
+from typing import Any
 
-def product_name(name:str,price:float,quantity:float)->None:
+def product_name(name:str,price:float,quantity:float)->str:
     return f"Nome prodotto: {name},prezzo: {price},quantità: {quantity}"
 
 def shopping_cart():
     return [products]
 
-products:list=[]
+products:dict={}
 
-select:str=input(" Insert: Add,remove or view products: ")
 
-match select:
-    case "add":
-        insert:str=input("Which product would you like to insert?")
+while True:
+    select:str=str(input(" Insert: Add,remove or view products: "))
+    match select:
+        case "add":
+            insert:str=input("Which product would you like to insert? ")
+            if insert=="apple":
+                products["apple"]=1.99
+            elif insert=="pineapple":
+                products["pineapple"]=2.67
+            elif insert=="tomatoes":
+                products["tomatoes"]=0.87
+        case "remove":
+            remove:str=input("Which product would you like to remove?: ")
+            removed_product= products.pop[remove]
+        case "view":
+            print(products)
