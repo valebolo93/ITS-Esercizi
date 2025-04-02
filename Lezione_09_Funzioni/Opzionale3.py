c5bdbac478751684599605257bd741f32df19f55
valebolo93 <valebolo93@gmail.com>
2025-04-02 17:17:15 +0200

    ex1

--- Lezione_09_Funzioni/Opzionale3.py
+++ Lezione_09_Funzioni/Opzionale3.py
@@ -5,13 +5,29 @@
     The function should calculate the cart total and apply any discounts or taxes.
      Implement a for loop to iterate over the items in the cart and print detailed information about each product and the total.
 '''
-from typing import Any
 
-def product_name(name:str,price:float,quantity:float)->str:
-    return f"Nome prodotto: {name},prezzo: {price},quantità: {quantity}"
+class Product:
 
-def shopping_cart():
-    return [products]
+    def __init__(self,name:str,price:float,quantity:float):
+        self.name=name
+        self.price=price
+        self.quantity=quantity
+
+
+
+    def product_name(self,variabile_sconto,taxes=22):
+        self.taxes=taxes
+        self.variabile_sconto=variabile_sconto
+        tax_value = self.price * (taxes / 100)
+        discount_value = self.price * tax_value * (variabile_sconto / 100)
+        return f"Nome prodotto: {self.name},prezzo: {self.price},quantità: {self.quantity},sconto: {discount_value},tasse: {taxes}"
+
+    def shopping_cart(self):
+        total_price = self.price * self.quantity
+        taxes = total_price * 0.22  
+        return sum(value),taxes
+    
+    print(shopping_cart)
 
 products:dict={}
 
@@ -25,10 +41,23 @@ while True:
                 products["apple"]=1.99
             elif insert=="pineapple":
                 products["pineapple"]=2.67
+            elif insert=="pear":
+                products["pear"]=0.45
+            elif insert=="banana":
+                products["banana"]=0.32
             elif insert=="tomatoes":
                 products["tomatoes"]=0.87
         case "remove":
             remove:str=input("Which product would you like to remove?: ")
-            removed_product= products.pop[remove]
+            removed_product= products.pop(remove)
         case "view":
             print(products)
+        case _:
+            exit:str=("Do you want to exit?: ")
+            if exit=="yes":
+                print(f"Totale:{value}")
+
+    for key,value in products.items():
+        print(value)
+
+        
