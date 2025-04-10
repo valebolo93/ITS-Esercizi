29f6130041e8058f48f640f9de2bda3df7b48d28
valebolo93 <valebolo93@gmail.com>
2025-04-10 13:23:15 +0200

    lezione8

--- Lezione-08/studente.py
+++ Lezione-08/studente.py
@@ -1,4 +1,5 @@
 from persona_giusto import Persona
+from typing import Any
 
 class Studente(Persona):
     
@@ -17,28 +18,64 @@ class Studente(Persona):
             self.matricola=matricola
         else:
             print("Errore! La matricola non può essere una stringa vuota")
-    
-    
-    
-    ''' 
-    attributi ereditati dalla classe Persona
-    self.name
-    self.last_name
-    self-age
 
-    attributi classe Studente:
-    self.matricola
-    
-    SOLO COSA DIDATTICA PER VERIFICARE
-    def inheritanceTest(self):
-        #verificare che la classe studente eredita tutti gli attributi della calsse Persona
-        self.name
-        self.last_name
-        self.age
+    def getMatricola(self)->str:
+        return self.matricola
 
-        #verificare che la classe studenti erediti tutti i metodi della classe Persona
-        self.getName()
-        self.getLastName()
-        self.getAge()
-'''
+        #metodo che consente di visualizzare le info relative all'oggetto della classe.
+        #overwriting
+    def __str__(self):
+        return f"\nNome: {self.getName()}\nCognome:{self.getLastName()}\nMatricola: {self.getMatricola()}"
+
+        #metodo che consente di calcolare la media degli esami sostenuti da uno studente
+
+    def getMediaEsami(self,voti_esami:list[int])->float:
+        #se la lista non è vuota
+        if voti_esami:
+            #calcolare la somma dei voti
+            somma:int=0
+            for voto in voti_esami:
+                somma+=voto
+
+            #ritornare la media
+            return round(somma/len(voti_esami),2)
+            #se la vista è vuota
+        else:
+            return 0.00
+        
+        #definire un metodo che consente di confrontare gli oggetti della classe Studente e stabilere se questi due oggetti siano uguali o meno
+    #due studenti sono uguali se hanno la stessa matricola
+    def __eq__(self, other)-> bool:
+        #Se other è None, oppure se other non è istanza della classe Studente, ritorna False
+
+        if other is None or not isinstance(other,type(self)):
+            return False
+        #altrimenti valuta la seguente uguaglianza
+        else:
+            return self.getMatricola()==other.getMatricola()
+
+
+
+    
+            ''' 
+            attributi ereditati dalla classe Persona
+            self.name
+            self.last_name
+            self-age
+
+            attributi classe Studente:
+            self.matricola
+            
+            SOLO COSA DIDATTICA PER VERIFICARE
+            def inheritanceTest(self):
+                #verificare che la classe studente eredita tutti gli attributi della calsse Persona
+                self.name
+                self.last_name
+                self.age
+
+                #verificare che la classe studenti erediti tutti i metodi della classe Persona
+                self.getName()
+                self.getLastName()
+                self.getAge()
+        '''
 
--- Lezione-08/test.py
+++ Lezione-08/test.py
@@ -10,4 +10,70 @@ p:Persona = Persona("Valentina","Bologna",32)
 print(p)
 #creare un oggetto studente1 di classe Studente
 studente1:Studente = Studente("Stefano", "Martelloni",32,"0797434567")
+
+#voglio controllare che studente sia istanza della classe studente
+
+if isinstance(studente1,Studente):
+    print("\nstudente1 è un oggetto della classe Studente")
+
+#la funzione isinstance(obj,Class)->True se l'obj è un istanza della classe Class
+
+#voglio sapere se studente1 è anche classe Persona visto che la classe Stud eredita dalla classe Persona
+
+if isinstance(studente1, Persona):
+      print("\nstudente1 è un'istanza della classe Persona")
+else:
+        print("\nstudente1 è  solo un'istanza della classe Studente e non della classe Persona")
+
+#voglio controllare che l'oggetto p sia un'istanza di PErsona
+
+if isinstance(p,Persona):
+      print("\np è un'istanza della classe Persona")
+
+    #voglio controllare che l'oggetto p sia un'istanza di Stud
+
+if isinstance(p,Studente):
+      print("\np è un'istanza della classe Persona e anche della classe Studente")
+else:
+      print("\np è solo un'istanza della classe Persona e non della classe Studente")
+      
+      #voglio controllare se la classe Studente è sottoclasse della classe Persona
+    #issubclass(Class1,Class2)->controllare se Class1 è sottoclasse della Class2: -> in caso affermativo ritorna True
+
+if issubclass(Studente,Persona):
+      print("\nLa class Studente è sottoclasse della Class Persona")
+
 print(studente1)
+print("-------------------------------------------------")
+print(p)
+
+print(f"\nLa media dei voti degli esami sostenuti dallo studente 1 è pari a: {studente1.getMediaEsami([10,20,30])}")
+
+#creiamo studente2
+
+studente2: Studente = Studente(p.getName(),p.getLastName(),p.getAge(),"0456789")
+
+print("-----------------------------------")
+print(studente2)
+
+#confrontare se studente1==p
+print("\n",studente1==p)
+
+#confronto studente 1 con studente 2
+print("\n",studente1==studente2)
+
+#verifichiamo che lo studente 1 sia uguare a se stesso
+print("\n",studente1==studente1)
+
+#modificare nome,cognome,età dello studente 2 affinchè abbia stesso nome,cognome ed età dello studente 1
+
+studente2.setName(studente1.getName())
+studente2.setLastName(studente1.getLastName())
+studente2.setAge(studente1.getAge())
+
+#provo di nuovo a confrontare studente1 e studente2
+print("\n",studente1==studente2)
+#ho forzato la matricola dello studente 2 ad essere uguale alla matricola dello studente1
+studente2.setMatricola(studente1.getMatricola())
+#confronto di nuovo studente1 constudente 2
+print("\n",studente1==studente2)
