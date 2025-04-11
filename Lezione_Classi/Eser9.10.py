e5bb0decfd374cd39cf79172643dd71a223512c3
valebolo93 <valebolo93@gmail.com>
2025-04-11 09:17:04 +0200

    es

---
+++ Lezione_Classi/Eser9.10.py
@@ -0,0 +1,6 @@
+'''Using your latest Restaurant class, store it in a module.
+ Make a separate file that imports Restaurant. Make a Restaurant instance, 
+ and call one of Restaurant’s methods to show that the import 
+ statement is working properly.'''
+
+
